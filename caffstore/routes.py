01c6314a72432b780bf12a74b01f57fff8c942b0
Wágner Árpád <wgnrar@gmail.com>
2022-12-04 12:02:42 +0100

    Put main part of logging in into a separate function so it can be called from elsewhere if needed

--- caffstore/routes.py
+++ caffstore/routes.py
@@ -86,6 +86,15 @@ def register():
 
     return render_template('register.html', title='Register', form=form)
 
+
+def do_login(user_email, user_pswd, remember) -> bool:
+    user = User.query.filter_by(email=user_email).first()
+    if user and bcrypt.check_password_hash(user.password, user_pswd):
+        login_user(user, remember=remember)
+        return True
+    return False
+
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -93,9 +102,8 @@ def login():
 
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
+        login_success = do_login(form.email.data, form.password.data, form.remember.data)
+        if login_success:
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
